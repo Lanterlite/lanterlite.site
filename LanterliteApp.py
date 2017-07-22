261e20eec2155390baf691bd7b4cd8ba76f58832
Ifan Ramadhani <ifan.lanterlite@gmail.com>
2017-07-22 16:46:19 +0700

    use flask-bootstrap

--- LanterliteApp.py
+++ LanterliteApp.py
@@ -1,6 +1,12 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('bootstrap_index.html')
+	
+if __name__ == '__main__':
+	app.run(debug=True)
